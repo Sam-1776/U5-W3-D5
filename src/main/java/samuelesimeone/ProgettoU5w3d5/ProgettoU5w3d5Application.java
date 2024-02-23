1705e97077abdac8ec3b8befa80c749b8f632a55
Samuele Simeone <69748013+Sam-1776@users.noreply.github.com>
2024-02-23 11:12:21 +0100

    First commit

---
+++ src/main/java/samuelesimeone/ProgettoU5w3d5/ProgettoU5w3d5Application.java
@@ -0,0 +1,13 @@
+package samuelesimeone.ProgettoU5w3d5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoU5w3d5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoU5w3d5Application.class, args);
+	}
+
+}
---
+++ src/test/java/samuelesimeone/ProgettoU5w3d5/ProgettoU5w3d5ApplicationTests.java
@@ -0,0 +1,13 @@
+package samuelesimeone.ProgettoU5w3d5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoU5w3d5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
